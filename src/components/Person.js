0d236ebd7c7a826d890e068f2652029f9cb6a5cf
Fernanda Vives <121441645+Fervivero@users.noreply.github.com>
2023-07-12 23:40:10 -0400

    Update Person.js

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,25 +1,36 @@
-import { Component } from "react";
+import React, { Component } from 'react';
 
 class Person extends Component {
-    render() {
-        const { firstName, lastName, age, hairColor } = this.props
-
-        const aumentarEdad = () => {
-            this.setAge({age: this.setAge + 1 });
+    constructor(props) {
+        super(props);
+        const { firstName, lastName, age, hairColor } = props;
+        this.state = {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            hairColor: hairColor
         }
+    }
 
+    aumentarEdad = () => {
+        this.setState({age: this.state.age + 1 });
+    }
+
+
+    render() {
         return (
             <div className="person">
-                <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
-                <button type="button" className="btn btn-success " onClick={aumentarEdad}> Birthday Button for {firstName} {lastName} </button>
+                <h1>{this.state.lastName}, {this.state.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.state.hairColor}</p>
+                <button type="button" className="btn btn-success " onClick={this.aumentarEdad}> Birthday Button for {this.state.firstName} {this.state.lastName} </button>
+                <hr></hr>
             </div>
-
 
 
         );
     }
-}
+
+    }
 
 export default Person;
